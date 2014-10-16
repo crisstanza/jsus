b04fa261289c5c4ada63d4e963771b5fc7b9c0c8
Cristiano S. Neves <cristiano.neves@datacom.ind.br>
2014-10-16 16:45:52 -0300

    Testing tool and use case.

--- js/JSUS.js
+++ js/JSUS.js
@@ -1,6 +1,41 @@
-function JSUS(testable) {
-  this.testable = testable;
-  function start() {
-    console.log('start ' + testable);
-  }
+function JSUS(testClass) {
+	this.testClass = testClass;
 }
+
+JSUS.prototype.start = function() {
+	{
+		console.log('JSUS.prototype.start');
+	}
+	var testObject = new this.testClass();
+	for (var testMethod in testObject) {
+		if (testMethod in testObject) {
+			var inTestNow = '['+testObject.constructor.name+'].'+testMethod+'()';
+			try {
+				testObject[testMethod]();
+				{
+					console.log('  '+inTestNow+' [SUCCESS]');
+				}
+			} catch (error) {
+				{
+					console.log('  '+inTestNow+' [FAIL] '+error.message);
+				}
+			}
+		}
+	}
+};
+
+JSUS.prototype.end = function() {
+	{
+		console.log('JSUS.prototype.end');
+	}
+}
+
+JSUS.assert = function(obj, msg) {
+	if (obj == false) {
+		throw new Error(msg);
+	}
+};
+
+JSUS.assertEquals = function(obj1, obj2) {
+	JSUS.assert(obj1 == obj2, '['+obj1+'] should be equals to ['+obj2+']')
+};
--- js/js.js
+++ js/js.js
@@ -1,14 +1,9 @@
 (function() {
 
-  function Testable () {
-    function testOne() {
-      console.log('test 1');
-    }
-  }
-
 	function init() {
 		var jsus = new JSUS(Testable);
 		jsus.start();
+		jsus.end();
 	}
 
 	window.addEventListener('load', init, false);
